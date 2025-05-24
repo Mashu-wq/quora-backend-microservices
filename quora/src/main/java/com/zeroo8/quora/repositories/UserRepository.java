bfcc2f143395741517050454289f337611eb5cf1
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-05-24 03:12:41 -0700

    created existsByEmail method

--- quora/src/main/java/com/zeroo8/quora/repositories/UserRepository.java
+++ quora/src/main/java/com/zeroo8/quora/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.zeroo8.quora.repositories;
 
 import com.zeroo8.quora.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existsByEmail(@NotBlank @Email String email);
 }
