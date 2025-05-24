851d78c9ef3d16b83e189237465f0b052dff9b4f
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-05-24 02:20:34 -0700

    implemented createUserRequest DTO

---
+++ quora/src/main/java/com/zeroo8/quora/dto/CreateUserRequest.java
@@ -0,0 +1,21 @@
+package com.zeroo8.quora.dto;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateUserRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    @Email
+    private String email;
+}
