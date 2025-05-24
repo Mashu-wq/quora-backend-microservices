edad81c3ed74d041d2a29286e09921113e152f96
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-05-24 02:22:16 -0700

    created updateUserRequest DTO

---
+++ quora/src/main/java/com/zeroo8/quora/dto/UpdateUserRequest.java
@@ -0,0 +1,16 @@
+package com.zeroo8.quora.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateUserRequest {
+    private String username;
+    private String email;
+    private String bio;
+}
