e0c494aa50f18ea449120d3696574107284ca393
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-05-24 02:22:38 -0700

    created UserAdapter

---
+++ quora/src/main/java/com/zeroo8/quora/adapter/UserAdapter.java
@@ -0,0 +1,32 @@
+package com.zeroo8.quora.adapter;
+
+import com.zeroo8.quora.dto.CreateUserRequest;
+import com.zeroo8.quora.dto.UpdateUserRequest;
+import com.zeroo8.quora.dto.UserResponse;
+import com.zeroo8.quora.models.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserAdapter {
+    public UserResponse toResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .bio(user.getBio())
+                .build();
+    }
+
+    public User toEntity(CreateUserRequest request) {
+        return User.builder()
+                .username(request.getUsername())
+                .email(request.getEmail())
+                .build();
+    }
+
+    public void updateEntity(User user, UpdateUserRequest request) {
+        if (request.getUsername() != null) user.setUsername(request.getUsername());
+        if (request.getEmail() != null) user.setEmail(request.getEmail());
+        if (request.getBio() != null) user.setBio(request.getBio());
+    }
+}
