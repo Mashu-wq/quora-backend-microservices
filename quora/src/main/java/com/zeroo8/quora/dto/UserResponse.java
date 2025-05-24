5ae2bf75c05447fbd0ee554adf9a472b9da8f960
Mayesha Marzia Zaman <mim2007088@stud.kuet.ac.bd>
2025-05-24 02:24:28 -0700

    created userResponse

---
+++ quora/src/main/java/com/zeroo8/quora/dto/UserResponse.java
@@ -0,0 +1,19 @@
+package com.zeroo8.quora.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+    private UUID id;
+    private String username;
+    private String email;
+    private String bio;
+}
